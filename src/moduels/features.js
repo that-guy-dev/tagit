e4e59cd1142a066613ec02cf4a59b37265bba804
Björn Lárus Arnórsson <bjornlarus@gmail.com>
2018-08-31 12:24:47 +0000

    Update features.js

--- src/moduels/features.js
+++ src/moduels/features.js
@@ -200,7 +200,7 @@ const Features = (props) => {
         <ScFeatureItemWrapper>
           <ScFeatureItem>
             <Recommended/>
-            <ScSubHeader>Recommandation</ScSubHeader>
+            <ScSubHeader>Recommendation</ScSubHeader>
           </ScFeatureItem>
           <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
         </ScFeatureItemWrapper>
