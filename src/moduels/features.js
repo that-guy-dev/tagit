918e77b936752770f98c372612057b2a6a95d8e2
Björn Lárus Arnórsson <bjornlarus@gmail.com>
2018-09-03 17:46:49 +0000

    features

--- src/moduels/features.js
+++ src/moduels/features.js
@@ -67,7 +67,7 @@ const ScSubHeader = styled.h3`
 `
 
 const ScP = styled.p`
-  padding: 15px 30px 40px 30px;
+  padding: 15px 30px 20px 30px;
   line-height: 26px;
   font-size: 1.1em;
 `
@@ -164,7 +164,7 @@ const Features = (props) => {
             <Save/>
             <ScSubHeader>Save and view later</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>With so much content on the internet and so little time, tagit can be your best friend in saving and then viewing content whenever you want at the right time for you.</ScP>
         </ScFeatureItemWrapper>
       </ScFeature>
 
@@ -174,7 +174,7 @@ const Features = (props) => {
             <Organize/>
             <ScSubHeader>Organize</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>We offer a very powerful tag system that helps you categorize and organize content for easy access later, no more lost articles that you wanted to read.</ScP>
         </ScFeatureItemWrapper>
       </ScFeature>
 
@@ -184,7 +184,7 @@ const Features = (props) => {
             <Reminder/>
             <ScSubHeader>Reminders</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>We all sometimes need a reminder. When you tag content we offer you the possibility to get a reminder for that piece of content, yes please next saturday would be great. </ScP>
         </ScFeatureItemWrapper>
       </ScFeature>
 
@@ -194,57 +194,58 @@ const Features = (props) => {
             <Highlight/>
             <ScSubHeader>Highlight</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>Remeber that good old highlight marker. This is the same. You can highlight parts of articles for fast access.</ScP>
+          {/* <ScP>Not all content is great, somtimes there is a bit here and there you want to highlight for fast access.</ScP> */}
         </ScFeatureItemWrapper>
       </ScFeature>
 
-          <ScFeature left pos="250">
+          {/* <ScFeature left pos="250">
         <ScFeatureItemWrapper>
           <ScFeatureItem>
             <Share/>
             <ScSubHeader>Share</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>Share your findings to social networks.</ScP>
         </ScFeatureItemWrapper>
-      </ScFeature>
+      </ScFeature> */}
 
-      <ScFeature right pos="300">
+      <ScFeature left pos="250">
         <ScFeatureItemWrapper>
           <ScFeatureItem>
             <Recommended/>
             <ScSubHeader>Recommendation</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>We analyze what you tag and give you recommendation on more articles to read based on your interests.</ScP>
         </ScFeatureItemWrapper>
       </ScFeature>
 
-         <ScFeature left pos="350">
+         <ScFeature right pos="300">
         <ScFeatureItemWrapper>
           <ScFeatureItem>
             <Collab/>
             <ScSubHeader>Collaborate</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>Share with other users and discover new content together. Create a tag directory for common interest or let others follow your discoveries</ScP>
+        </ScFeatureItemWrapper>
+      </ScFeature>
+
+      <ScFeature left pos="350">
+        <ScFeatureItemWrapper>
+          <ScFeatureItem>
+            <Central/>
+            <ScSubHeader>Centralized</ScSubHeader>
+          </ScFeatureItem>
+          <ScP>All of your findings and discoveries will be accessible on every device.</ScP>
         </ScFeatureItemWrapper>
       </ScFeature>
 
       <ScFeature right pos="400">
         <ScFeatureItemWrapper>
           <ScFeatureItem>
-            <Central/>
-            <ScSubHeader>Centralized</ScSubHeader>
-          </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
-        </ScFeatureItemWrapper>
-      </ScFeature>
-
-      <ScFeature left pos="450">
-        <ScFeatureItemWrapper>
-          <ScFeatureItem>
             <Offline/>
             <ScSubHeader>Offline access</ScSubHeader>
           </ScFeatureItem>
-          <ScP>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</ScP>
+          <ScP>Everything you tag will be save and secure for as long as you please. When there is no signal, you can be sure that all of your content will still be accessible.</ScP>
         </ScFeatureItemWrapper>
       </ScFeature>
 
